e15de7cf51efd36510f5d4564fb2b5cde541dad1
Aurélie PREAUD <aureliepreaud@MacBook-Air-de-Aurelie.local>
2022-12-27 14:24:02 +0100

    Migration Heroku vers Vercel

--- src/App.js
+++ src/App.js
@@ -21,7 +21,8 @@ import Footer from "./components/Footer";
 
 //variable URL
 // const url = "http://localhost:3001/";
-const url = "https://lily-vinted.herokuapp.com/";
+//const url = "https://lily-vinted.herokuapp.com/";
+const url = "https://vinted-eight.vercel.app/";
 
 function App() {
   const [userToken, setUserToken] = useState(Cookies.get("userToken") || null);
